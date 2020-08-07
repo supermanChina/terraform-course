083fe75693ba2d4a7396b5dc7dee397dd932770d
Xiaochao Zhang <xiaochao.zhang@siemens.com>
2020-08-07 15:35:13 +0800

    self practice for Terraform.

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,10 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  profile = "default"
+  region   = "us-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-0d705db840ec5f0c5"
   instance_type = "t2.micro"
 }
 
